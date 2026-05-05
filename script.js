c757757ec7d66c5ce9bf684ced1298a7cb6f164e
lucasmoraes2407-sketch <lucas.moraes2407@aluno.seduc.pa.gov.br>
2026-05-05 16:05:44 -0300

    Add game logic for player movement and responses

--- script.js
+++ script.js
@@ -1,7 +1,71 @@
+let faseAtual = 0;
+
+// POSIÇÕES DOS 10 BLOQUINHOS
+// x = esquerda
+// y = topo
+const posicoes = [
+
+    {x:35,  y:640},
+    {x:70,  y:610},
+    {x:105, y:565},
+    {x:135, y:520},
+    {x:165, y:470},
+    {x:195, y:420},
+    {x:220, y:365},
+    {x:245, y:300},
+    {x:270, y:235},
+    {x:290, y:170}
+
+];
+
 function iniciarJogo(){
 
     document.getElementById("telaInicial").style.display = "none";
-
     document.getElementById("game").style.display = "block";
 
+    moverPersonagem();
 }
+
+
+// COLOCA O BONECO NO BLOCO CERTO
+function moverPersonagem(){
+
+    const player = document.getElementById("player");
+
+    player.style.left = posicoes[faseAtual].x + "px";
+    player.style.top = posicoes[faseAtual].y + "px";
+}
+
+
+// CLIQUE NAS RESPOSTAS
+const botoes = document.querySelectorAll("#opcoes button");
+
+botoes.forEach(botao => {
+
+    botao.onclick = function(){
+
+        const resposta = parseInt(this.innerText);
+
+        // resposta certa = 4
+        if(resposta === 4){
+
+            if(faseAtual < 9){
+
+                faseAtual++;
+                moverPersonagem();
+
+            }else{
+
+                alert("Parabéns! Você completou as 10 fases!");
+
+            }
+
+        }else{
+
+            alert("Resposta errada!");
+
+        }
+
+    }
+
+});
